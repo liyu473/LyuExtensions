42bcaae5be959bcea873f734b514e0a44077d39b
lizhenyu <2945659544@qq.com>
2025-11-17 16:59:04 +0800

    字符串拓展更新以便去除警告

--- StringExtensions.cs
+++ StringExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Extensions;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Extensions;
 
 public static class StringExtensions
 {
@@ -7,12 +9,17 @@ public static class StringExtensions
     /// </summary>
     /// <param name="value">要检查的字符串。</param>
     /// <returns>如果字符串为 null 或仅包含空白字符，则返回 true；否则返回 false。</returns>
-    public static bool IsNullOrWhiteSpace(this string? value)
+    public static bool IsNullOrWhiteSpace([NotNullWhen(false)] this string? value)
     {
         return string.IsNullOrWhiteSpace(value);
     }
 
-    public static bool IsNullOrEmpty(this string? value)
+    /// <summary>
+    /// 判断字符串是否为 null 或空。
+    /// </summary>
+    /// <param name="value">要检查的字符串。</param>
+    /// <returns>如果字符串为 null 或空，则返回 true；否则返回 false。</returns>
+    public static bool IsNullOrEmpty([NotNullWhen(false)] this string? value)
     {
         return string.IsNullOrEmpty(value);
     }
